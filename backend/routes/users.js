fe5ea6d2c1827d1fd20dfaa8ef04b5d95e9ea554
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-10-18 13:40:59 +0200

    add .gitignore file into project root

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(JSON.stringify({text: 'test data from node.js' + Date.now()}));
+  res.send(JSON.stringify({text: 'test data from node.js at ' + new Date()}));
 });
 
 module.exports = router;
